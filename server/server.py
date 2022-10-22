c2e2356d6a29c15c1a28756d38631266b9c63a21
Chandan Singh <csrrocz@gmail.com>
2022-10-22 22:06:09 +0200

    making list of dictionary

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 from flask import Flask, request, render_template, Response
 from Vector import Vector
+import pymongo
 
 from helper import *
 
@@ -17,7 +18,12 @@ global map_table
 
 
 
-map_table = {}
+map_table = []
+
+def create_connection(db_file):
+    client = pymongo.MongoClient("mongodb+srv://<username>:abcd1234@cluster0.spfiabd.mongodb.net/?retryWrites=true&w=majority")
+    mydb = client[db_file]
+    return mydb
 
 
 
@@ -28,30 +34,38 @@ def save_data():
         Check if table is empty
         GET all keys
     """
-    N = len(map_table.keys()) # Number of entries
+    """ database initilization"""
+    database = "myDatabase"
+    mydb = create_connection(database)
+    
+    mycol = mydb["myCollection"]
+    
+    N = len(map_table) # Number of entries
     chunk = json.loads(request.get_json())
     
     if not N: # Check number of entries
         N += 1
-        map_table[N] = {"key": chunk, "data": [chunk]} # Add the first entry of the table
+        map_table.append({"key": chunk, "data": [chunk]}) # Add the first entry of the table
 
     else:
-        entries = map_table.values() # Get entries e.g entry = {1: {"key": [list], "data": [[list of lists]] }}
+        entries = map_table # Get entries e.g entry = {1: {"key": [list], "data": [[list of lists]] }}
         keys = [ entry["key"] for entry in entries] # Get keys from map table (list of lists)
 
         # Compute similarity chunk with respect to all keys in table
         max_sim, max_sim_key, max_sim_entry = maximum_similarity(chunk=np.array(chunk), keys= np.array(keys))
 
         if max_sim > THRESHOLD: # Check if max_sim value is above threshold
-            map_table[max_sim_entry]["data"].append(chunk) # Append chunk data to the key that gives highest similarity (max_sim_key)
+            map_table[max_sim_entry-1]["data"].append(chunk) # Append chunk data to the key that gives highest similarity (max_sim_key)
         else:
             N += 1 # Increment number of entries in the table
-            map_table[N] = {"key": chunk, "data": [chunk]} # Create a new entry with the new chunk as a key
+            map_table.append({"key": chunk, "data": [chunk]}) # Create a new entry with the new chunk as a key
 
         
     
-    count_data = {k: len(v["data"]) for (k, v) in map_table.items()}
+    # count_data = {k: len(v["data"]) for (k, v) in map_table}
+    count_data = {map_table.index(item): len(item["data"]) for item in map_table}
     print(count_data)
+    print(map_table)
 
     return Response(response=json.dumps("Chunk received"), mimetype="application/json", status=200)
 
